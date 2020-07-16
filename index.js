29079aa7e5c1eee73138c1397d066f1eb68182b8
Manpreet Kaur <kaurman2305@gmail.com>
2020-07-16 15:06:37 +0530

    Dice Game

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomNumber2=Math.floor(Math.random()*6)+1;
+var winner;
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML="🚩️Player 1 wins!";
+}
+else if(randomNumber1<randomNumber2){
+document.querySelector("h1").innerHTML="Player 2 wins!🚩️";
+}
+else{
+  document.querySelector("h1").innerHTML="Draw!";
+}
+
+document.querySelector("img.img1").setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector("img.img2").setAttribute("src","images/dice"+randomNumber2+".png");
